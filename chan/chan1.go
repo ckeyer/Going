e9e8de32ed14c9dcadcbba9338cd4ef1b9e95b0d
ckeyer <wangcj1214@gmail.com>
2015-03-29 23:26:56 +0800

    add chan_For

--- chan/chan1.go
+++ chan/chan1.go
@@ -6,9 +6,43 @@ import (
 )
 
 func sleep(second int) {
-	time.Sleep(second * time.Second)
+	time.Sleep((time.Duration)(second) * time.Second)
 }
 
 func main() {
+	test1()
+}
 
+func test1() {
+	c := make(chan int, 1) //
+	// defer
+	go test1ChanGet(c)
+	go test1ChanSet(c, 11)
+	go test1ChanSet(c, 22)
+	go test1ChanSet(c, 44)
+	sleep(3)
+	go test1ChanGet(c)
+	go test1ChanSet(c, 33)
+	// fmt.Println(c)
+	sleep(5)
+	close(c)
+	if c != nil {
+		fmt.Println("sdf")
+		for e := range c {
+			fmt.Println("end : ", e)
+		}
+	}
+
+}
+
+func test1ChanGet(c <-chan int) {
+
+	sleep(2)
+	fmt.Println("get :", <-c)
+}
+func test1ChanSet(c chan<- int, ci int) {
+	fmt.Println("before set:", ci)
+	c <- ci
+
+	fmt.Println("set over:", ci)
 }
