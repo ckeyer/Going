7d5a510bfca299a6644c3dd04d0a66177cb2e7bd
ckeyer <wangcj1214@gmail.com>
2015-03-31 23:42:34 +0800

    test copy

--- init_test/b.go
+++ init_test/b.go
@@ -1,24 +1,39 @@
 package main
 
-import logpkg "github.com/funxdata/log4go"
-
-var (
-	log logpkg.Logger
+import (
+	"fmt"
 )
 
-func init() {
-	log = logpkg.NewConsoleLogger(logpkg.WARNING)
-}
 func main() {
-	log.Error("hh")
-	for i := 1; i <= 100; i++ {
-		log.Info(i, " ")
-		if 0 == i%3 {
-			log.Fine("Fizz")
-		}
-		if 0 == i%5 {
-			log.Warn("Buzz")
-		}
-		log.Warn("")
-	}
+	bs := make([]byte, 8)
+
+	copy(bs, []byte("1234"))
+
+	copy(bs[4:], []byte("5678"))
+
+	fmt.Printf("%s\n", bs)
+
 }
+
+// import logpkg "github.com/funxdata/log4go"
+
+// var (
+// 	log logpkg.Logger
+// )
+
+// func init() {
+// 	log = logpkg.NewConsoleLogger(logpkg.WARNING)
+// }
+// func main() {
+// 	log.Error("hh")
+// 	for i := 1; i <= 100; i++ {
+// 		log.Info(i, " ")
+// 		if 0 == i%3 {
+// 			log.Fine("Fizz")
+// 		}
+// 		if 0 == i%5 {
+// 			log.Warn("Buzz")
+// 		}
+// 		log.Warn("")
+// 	}
+// }
