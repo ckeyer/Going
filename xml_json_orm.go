93a2875342385f345e33749c7315aaddf7f3fddc
王传健 <ckeyer@users.noreply.github.com>
2015-04-19 23:57:17 +0800

    up orm_json_xml

--- xml_json_orm.go
+++ xml_json_orm.go
@@ -5,16 +5,41 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 type TestUser struct {
 	Id    int64      `json:"-",xml:"-"`
 	Name  string     `json:"name",xml:"Name",orm:"size(16)"`
 	Class *TestClass `orm:"one"`
+	Created time.Time `orm:"auto_now_add;type(datetime)"`
+	Updated time.Time `orm:"auto_now;type(datetime)"`
 }
 type TestClass struct {
 	Id   int64  `json:"-",xml:"-"`
 	Name string `json:"name",xml:"Name",orm:"size(16)"`
+	Created time.Time `orm:"auto_now_add;type(datetime)"`
+	Updated time.Time `orm:"auto_now;type(datetime)"`
+}
+
+func init(){
+	
+	orm.RegisterDriver("mysql", orm.DR_MySQL)
+	orm.RegisterDataBase("default", "mysql", "ckeyer:wangcj@/db_test?charset=utf8", 10, 10)
+
+	orm.RegisterModelWithPrefix("tb_", new(TestClass))
+	orm.RegisterModelWithPrefix("tb_", new(TestUser))
+	
+	
+	name := "default"
+	force := true
+	verbose := true
+	err := orm.RunSyncdb(name, force, verbose)
+	if err != nil {
+		CKLog.Error("create table false")
+		return
+	}
 }
 
 func main() {
@@ -32,4 +57,25 @@ func main() {
 	fmt.Printf("%s\n", bsjson)
 	fmt.Printf("%s\n", bsxml)
 
+}
+
+
+func (this *TestUser) Update() error {
+	o := orm.NewOrm()
+	_, e := o.Update(this)
+	if e != nil {
+		return e
+	}
+
+	return nil
+}
+
+func (this *TestClass) Update() error {
+	o := orm.NewOrm()
+	_, e := o.Update(this)
+	if e != nil {
+		return e
+	}
+
+	return nil
 }
